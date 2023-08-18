461efc0c87d2302724d57e9223fc89788b0d7e5b
Sajad.gm <74602302+sajadgm@users.noreply.github.com>
2023-08-18 15:45:40 +0330

    main task

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from 'react'
+import Products from './shared/products';
 import './App.css'
+import './shared/products.scss';
+
 
 function App() {
+
+	const[isLoading , setIsLoading] = useState(true)
+	const[myData , setMyData] = useState()
 	
+	
+	useEffect(() =>{
+		getData();
+	},[])
+
+	const getData = async () => {
+		const res = await fetch('http://localhost:3000/posts');
+		const data = await res.json();
+		setIsLoading(false)
+		return setMyData(data);
+	}
+
+	const removePost = (id) => {
+		fetch(`http://localhost:3000/posts/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		
+		})
+		.then(response => getData())
+		.catch(error => console.error(error));
+
+	}
+
 	return (
-		<div>
-			<h2>For instructions, see the README file</h2>
-		</div>
+		<>
+		{
+			!isLoading ? <Products data={myData} remove={removePost} setData={setMyData} /> : ''
+		}
+		</>
 	)
 }
 
---
+++ src/shared/products.jsx
@@ -0,0 +1,67 @@
+import { LiaCommentAltSolid  } from 'react-icons/lia';
+import { AiOutlineEye  } from 'react-icons/ai';
+import { FcLike  } from 'react-icons/fc';
+import { PiShareLight  } from 'react-icons/pi';
+import { useEffect, useState } from 'react';
+
+
+
+const  Products = (props) => {
+    const [incommingData , setIncommingData] = useState()
+
+    useEffect(() => {
+        setIncommingData(props.data)
+    },[props.data])
+
+    
+  
+
+    const likeCounter = (id) => {
+        return 1 + 4
+
+    }
+
+    const date = (timeStamp) =>{
+        return new Intl.DateTimeFormat('fa-IR', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timeStamp)
+
+    }
+
+    
+
+	return (
+        <>
+                { incommingData ?
+                incommingData.map((item, key) => {
+                    return  <div key={item.id} className='product-card'>
+                    <section className='product-card__title'>
+                        <div className='product-card__title--name'>
+                            <b>{item.name +' '+ item.username}</b>
+                            <a>{item.username}</a>
+                        </div>
+                        <img className='product-card__title--profileImg' src='./src/assets/profile.jpg'/>
+                    </section>
+                    <section className='product-card__content'>
+                        <img className='product-card__content--previewImg' src="./src/assets/postImage.jpg"></img>
+                        <div className='product-card__content--info'>
+                            <div className="product-card__content--info--activity">
+                                <div><AiOutlineEye size={22}/>2</div>
+                                <div><PiShareLight size={22}/>0</div>
+                                <div><LiaCommentAltSolid size={22}/>0</div>
+                                <div><FcLike size={22}/>{likeCounter(item.id)}</div>
+                            </div>
+                            <div className="product-card__content--info--caption">
+                                    <p>{date(item.timestamp)}</p>
+                                    <p>{item.text}</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className='product-card__buttons'>
+                    <button onClick={() => props.remove(item.id)}>حذف پست</button>
+                    </section>
+                </div>
+                }) : ''}
+        </>
+	)
+}
+
+export default Products
